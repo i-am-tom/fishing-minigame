7e6fedf27591632666f504b41e3f73b1d565ec24
Tom Harding <i.am.tom.harding@gmail.com>
2024-08-24 22:18:27 +0200

    Run elm-format as part of prettier

--- source/Main.elm
+++ source/Main.elm
@@ -4,25 +4,28 @@ import Browser exposing (element)
 import Html exposing (..)
 import State exposing (State)
 
+
 main : Program () State ()
 main =
-  element
-    { init = \_ ->
-        ( State.initial
-        , Cmd.none
-        )
-
-    , view = \state ->
-        let
-          money = String.fromFloat state.money
-        in
-          text ("You have $" ++ money ++ ".")
-
-    , update = \_ model ->
-        ( model
-        , Cmd.none
-        )
-
-    , subscriptions = \_ ->
-        Sub.none
-    }
+    element
+        { init =
+            \_ ->
+                ( State.initial
+                , Cmd.none
+                )
+        , view =
+            \state ->
+                let
+                    money =
+                        String.fromFloat state.money
+                in
+                text ("You have $" ++ money ++ ".")
+        , update =
+            \_ model ->
+                ( model
+                , Cmd.none
+                )
+        , subscriptions =
+            \_ ->
+                Sub.none
+        }
