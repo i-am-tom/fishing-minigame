4114950a068e380d6ecc091d8cf90bde6d851bc9
Tom Harding <i.am.tom.harding@gmail.com>
2024-08-25 12:14:24 +0200

    Oops

---
+++ source/View/Controls.elm
@@ -0,0 +1,26 @@
+module View.Controls exposing (controls)
+
+import Css exposing (..)
+import Html.Styled exposing (..)
+import Html.Styled.Attributes exposing (css)
+import Html.Styled.Events exposing (onClick)
+import State exposing (State)
+import State.Delta exposing (Delta)
+import Update exposing (Event(..))
+
+
+controls : State -> Html Event
+controls state =
+    div
+        [ css
+            [ flexGrow (int 1)
+            , padding (px 10)
+            ]
+        ]
+        [ button [ onClick CatchFish ]
+            [ text "Catch fish"
+            ]
+        , button [ onClick SellFish ]
+            [ text "Sell fish"
+            ]
+        ]
