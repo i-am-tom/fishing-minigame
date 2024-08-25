4f21009dea6edccb0774fac85a9f76c8e608b44f
Tom Harding <i.am.tom.harding@gmail.com>
2024-08-25 12:14:57 +0200

    Implement the start of a UI (#13)
    Nothing too clever yet, it just seemed to be a bit bland.

--- source/View.elm
+++ source/View.elm
@@ -1,28 +1,89 @@
 module View exposing (view)
 
-import Html exposing (..)
-import Html.Events exposing (onClick)
+import Css exposing (..)
+import Html.Styled exposing (..)
+import Html.Styled.Attributes exposing (css, src)
+import Html.Styled.Events exposing (onClick)
 import State exposing (State)
 import State.Delta exposing (Delta)
 import Update exposing (Event(..))
+import View.Controls exposing (controls)
+import View.Statistics exposing (statistics)
 
 
 view : Delta -> State -> Html Event
 view delta state =
-    let
-        fish =
-            String.fromFloat state.fish
+    div
+        [ css
+            [ alignItems center
+            , backgroundColor (hex "FCFCFC")
+            , displayFlex
+            , flexDirection column
+            , width (vw 100)
+            ]
+        ]
+        [ navigation
+        , mainContainer
+            [ statistics delta state
+            , controls state
+            ]
+        ]
 
-        money =
-            String.fromFloat state.money
-    in
-    div []
-        [ button [ onClick CatchFish ]
-            [ text "Catch fish"
+
+navigation : Html Event
+navigation =
+    div
+        [ css
+            [ justifyContent center
+            , borderBottom3 (px 1) solid (hex "BDBDBD")
+            , boxSizing borderBox
+            , boxShadow4 (px 0) (px 2) (px 4) (hex "BDBDBD")
+            , displayFlex
+            , position sticky
+            , width (vw 100)
             ]
-        , button [ onClick SellFish ]
-            [ text "Sell fish"
+        ]
+        [ div
+            [ css
+                [ alignItems center
+                , boxSizing borderBox
+                , displayFlex
+                , fontFamilies [ "Pacifico", "sans-serif" ]
+                , justifyContent spaceBetween
+                , padding (px 10)
+                , maxWidth (px 1000)
+                , width (pc 100)
+                ]
             ]
-        , p [] [ text ("You have " ++ fish ++ "kg of fish.") ]
-        , p [] [ text ("You have $" ++ money ++ ".") ]
+            [ h1
+                [ css
+                    [ fontSize (px 24)
+                    , margin (px 0)
+                    ]
+                ]
+                [ text "The Fishing Minigame"
+                ]
+            , img
+                [ css
+                    [ height (px 40)
+                    , width (px 40)
+                    ]
+                , src "assets/logo.svg"
+                ]
+                []
+            ]
+        ]
+
+
+mainContainer : List (Html Event) -> Html Event
+mainContainer =
+    main_
+        [ css
+            [ displayFlex
+            , flexDirection row
+            , flexGrow (int 1)
+            , fontFamilies [ "Roboto", "sans-serif" ]
+            , maxWidth (px 800)
+            , width (vw 100)
+            ]
         ]
---
+++ source/View/Statistics.elm
@@ -0,0 +1,107 @@
+module View.Statistics exposing (statistics)
+
+import Css exposing (..)
+import Html.Styled exposing (..)
+import Html.Styled.Attributes exposing (css)
+import NumberSuffix exposing (standardConfig, suffixStandardShort)
+import State exposing (State)
+import State.Delta exposing (Delta)
+import Update exposing (Event(..))
+
+
+statistics : Delta -> State -> Html Event
+statistics delta state =
+    ul
+        [ css
+            [ listStyleType none
+            , margin (px 0)
+            , padding (px 0)
+            , maxWidth (px 300)
+            , width (vw 40)
+            ]
+        ]
+        (List.map (renderDisplay delta state) displays)
+
+
+type alias Display =
+    { label : String
+    , color : Color
+    , shouldShow : State -> Bool
+    , current : State -> Float
+    , gradient : Delta -> Float
+    , format : String -> String
+    }
+
+
+displays : List Display
+displays =
+    [ { label = "Fish"
+      , color = rgb 79 79 79
+      , shouldShow = \_ -> True
+      , current = .fish
+      , gradient = .fish
+      , format = \s -> s ++ "kg"
+      }
+    , { label = "Money"
+      , color = rgb 33 150 83
+      , shouldShow = \_ -> True
+      , current = .money
+      , gradient = .money
+      , format = \s -> "$" ++ s
+      }
+    ]
+
+
+renderDisplay : Delta -> State -> Display -> Html Event
+renderDisplay delta state display =
+    let
+        formatNumber : Float -> String
+        formatNumber =
+            NumberSuffix.format
+                { standardConfig
+                    | getSuffix = suffixStandardShort
+                    , sigfigs = 3
+                    , minSuffix = 1000
+                }
+
+        gradient : Float
+        gradient =
+            display.gradient delta
+    in
+    li
+        [ css
+            [ boxSizing borderBox
+            , displayFlex
+            , fontSize (px 13)
+            , flexDirection column
+            , height (px 55)
+            , justifyContent spaceBetween
+            , borderBottom3 (px 1) solid (hex "E0E0E0")
+            , padding (px 10)
+            , overflowX hidden
+            ]
+        ]
+        [ strong
+            [ css
+                [ color display.color
+                ]
+            ]
+            [ text display.label
+            ]
+        , div []
+            [ strong []
+                [ display.current state
+                    |> formatNumber
+                    |> display.format
+                    |> text
+                ]
+            , if gradient > 0 then
+                text (" + " ++ (gradient |> formatNumber |> display.format) ++ "/s")
+
+              else if gradient == 0 then
+                text ""
+
+              else
+                text (" - " ++ (gradient |> abs |> formatNumber |> display.format) ++ "/s")
+            ]
+        ]
